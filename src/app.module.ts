cb04664bba13bfa690384c18cf89d7eb67d012ef
laurovcn <laurovcn@gmail.com>
2025-06-04 08:00:10 -0300

    fix import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController, HealthController } from './app.controller';
-import { DashboardController } from './dashboard.controller';
+import { DashboardModule } from './dashboard.module';
 import { FarmerModule } from './farmer/farmer.module';
 import { PropertyModule } from './property/property.module';
 import { HarvestModule } from './harvest/harvest.module';
@@ -8,8 +8,14 @@ import { CropModule } from './crop/crop.module';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [FarmerModule, PropertyModule, HarvestModule, CropModule],
-  controllers: [AppController, HealthController, DashboardController],
+  imports: [
+    FarmerModule,
+    PropertyModule,
+    HarvestModule,
+    CropModule,
+    DashboardModule,
+  ],
+  controllers: [AppController, HealthController],
   providers: [PrismaService],
 })
 export class AppModule {}
