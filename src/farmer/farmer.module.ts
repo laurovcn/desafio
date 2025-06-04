dc751f995bce8f28487d1b2461cb28c50b6107c5
laurovcn <laurovcn@gmail.com>
2025-06-03 22:57:36 -0300

    Add FarmerModule with controller and service registration

---
+++ src/farmer/farmer.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { FarmerController } from './farmer.controller';
+import { FarmerService } from './farmer.service';
+
+@Module({
+  controllers: [FarmerController],
+  providers: [FarmerService, PrismaService],
+})
+export class FarmerModule {}
