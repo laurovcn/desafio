e05f57f8cc7babec3f6ffa0f3b5966ef16a75a4e
laurovcn <laurovcn@gmail.com>
2025-06-03 22:57:22 -0300

    Add main.ts for NestJS bootstrap and Swagger setup

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.simple(),
+          ),
+        }),
+      ],
+    }),
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Gestão de Produtores Rurais')
+    .setDescription(
+      'API para gestão de produtores, propriedades, safras e culturas',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
